224059089bfee131a8c5028845b798929ab2fada
degschta <31890275+degschta@users.noreply.github.com>
2021-12-26 19:06:54 +0100

    Delete build directory

--- build/a1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lukas/robotik-projekt-gruppe-1/src/a1
-# Build directory: /home/lukas/robotik-projekt-gruppe-1/build/a1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/coppeliasim_ros_control/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rosconsole;roslib;roscpp;rosconsole;roslib".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "coppeliasim_ros_control"
-PROJECT_SPACE_DIR = "/home/lukas/a1_sim_py/devel"
-PROJECT_VERSION = "1.0.0"
